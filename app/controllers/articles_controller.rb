d1701df01d109adc7786aabefcda66d6befcf11e
Rich <rich@datarich.org>
2024-03-21 17:38:49 -0400

    added articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,9 @@ class ArticlesController < ApplicationController
     #binding.break
     @article = Article.find(params[:id])
   end
+
+  def index
+    #binding.break
+    @articles = Article.all
+  end
 end
